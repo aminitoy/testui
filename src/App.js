b1f605a8e4e1f171656174c248c5070bc5d4d77b
Amin Chirazi <aminchirazi@Amins-MacBook-Pro-2.local>
2022-07-18 20:33:28 +0200

    icon tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { AddIcon, Button, CalendarIcon, CallIcon, Menu } from "@fluentui/react-northstar";
+import { AppsIcon, BellIcon, ChatIcon, EditIcon, MeetingTimeIcon, SaveIcon } from '@fluentui/react-icons-northstar/dist/commonjs';
+import { Icon } from "@fluentui/react";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>ABCD</Button>
+        <Button text="Primary" allowDisabledFocus />
+        <Menu
+          items={
+            [
+              "1", "2", <EditIcon />, <AppsIcon />, <MeetingTimeIcon />, <SaveIcon />, <BellIcon />, <ChatIcon />, <AddIcon />, <CalendarIcon />, <CallIcon />
+            ]
+          }
+        />
+
+
+        <Icon iconName='CompassNW' />
+
       </header>
     </div>
   );
 }
 
 export default App;
+
+// Example formatting
+const stackTokens = { childrenGap: 40 };
+
+
