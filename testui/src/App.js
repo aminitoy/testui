162fdb650aab9d249ebb9f9725b458c283e008d0
Amin Chirazi <aminchirazi@Amins-MBP-2.fritz.box>
2022-07-20 20:00:54 +0200

    react-northstar menu

--- testui/src/App.js
+++ testui/src/App.js
@@ -1,21 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { Provider as TeamsProvider } from "@fluentui/react-teams";
-import { Chart, Dashboard, Board, Form, Table } from "@fluentui/react-teams";
+import { Input, Menu, Provider, teamsTheme, Icon } from "@fluentui/react-northstar";
 
 function App() {
   return (
     <div className="App">
+      <Input />
+      <Provider theme={teamsTheme}>
+        <Menu vertical>
+          <Menu.Item Icon="add" text="." />
+          <Menu.Item icon="edit" text="Edit" />
+          <Menu.Item icon="delete" text="Delete" />
+        </Menu>
+      </Provider>
 
 
-      <TeamsProvider themeName="teamsTheme" lang="en-US">
-
-        {/* <List items={[{ key: 'a' }, { key: 'b' }, { key: 'c' }]} /> */}
-<Board />
-
-
-
-      </TeamsProvider>
     </div>
   );
 }
