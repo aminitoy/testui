483c4bf79d25036d7c143d370300384b63eb7d59
Amin Chirazi <aminchirazi@amins-mbp-2.cnt-online.at>
2022-07-21 16:26:50 +0200

    simplify structure

--- testui/src/App.js
+++ testui/src/App.js
@@ -1,66 +1,191 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button, Menu } from "@fluentui/react-northstar";
+// import logo from './logo.svg';
+// import './App.css';
+// import { Button, Menu, menuAsToolbarBehavior } from "@fluentui/react-northstar";
+// import {
+//   CalendarIcon, ContactGroupIcon, UserFriendsIcon,
+//   SettingsIcon, PaperclipIcon, EyeIcon, ShiftActivityIcon, RobotIcon,
+// } from "@fluentui/react-icons-northstar";
+
+// const iconStyles = {
+//   color: '#482B7C',
+//   selected: {
+//     color: '#0078D4',
+//   },
+// }
+
+// const items = [
+//   {
+//     key: '0',
+//     content: 'Planning',
+//     icon: <CalendarIcon outline styles={iconStyles}/>,
+//     link: '#',
+//   },
+//   {
+//     key: 'overview',
+//     content: 'Overview',
+//     icon: <EyeIcon outline styles={iconStyles} />,
+//   },
+//   {
+//     key: 'times',
+//     content: 'Times',
+//     icon: <ShiftActivityIcon outline styles={iconStyles} />,
+//   },
+//   {
+//     content: 'Users',
+//     icon: <ContactGroupIcon outline styles={iconStyles} />,
+//   },
+//   {
+//     key: 'clients',
+//     content: 'Clients',
+//     icon: <UserFriendsIcon outline styles={iconStyles} />,
+//   },
+//   {
+//     key: 'services',
+//     content: 'Services',
+//     icon: <RobotIcon outline styles={iconStyles} />,
+//   },
+//   {
+//     key: 'invoices',
+//     content: 'invoices',
+//     icon: <PaperclipIcon outline styles={iconStyles} />,
+//   }
+// ]
+
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <Menu defaultActiveIndex={0} items={items} pills vertical
+//         accessibility={menuAsToolbarBehavior}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react'
+import { Menu, menuAsToolbarBehavior } from '@fluentui/react-northstar'
 import {
-  CalendarIcon, ContactGroupIcon, UserFriendsIcon,
-  SettingsIcon, PaperclipIcon, EyeIcon, ShiftActivityIcon, RobotIcon
-} from "@fluentui/react-icons-northstar";
-
-const iconStyles = {
-
-
-  color: '#482B7C',
-  ":hover": {
-    color: '#482B7C'
-  }
-
-
-}
+  FormatIcon,
+  PaperclipIcon,
+  EmojiIcon,
+  GiphyIcon,
+  StickerIcon,
+  VideoCameraEmphasisIcon,
+  SettingsIcon,
+  MoreIcon,
+  BookmarkIcon,
+  TranslationIcon,
+  TrashCanIcon,
+  MarkAsUnreadIcon,
+  UserFriendsIcon,
+  CalendarIcon,
+  EyeIcon,
+  ContactGroupIcon,
+  ShiftActivityIcon,
+  RobotIcon,
+} from '@fluentui/react-icons-northstar'
 
 const items = [
   {
+    icon: (
+      <CalendarIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
     key: 'planning',
     content: 'Planning',
-    icon: <CalendarIcon styles={iconStyles} />,
+    'aria-label': 'Planning',
   },
   {
+    icon: (
+      <EyeIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
     key: 'overview',
     content: 'Overview',
-    icon: <EyeIcon styles={iconStyles} />,
+    'aria-label': 'overview',
   },
   {
+    icon: (
+      <ShiftActivityIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
     key: 'times',
     content: 'Times',
-    icon: <ShiftActivityIcon styles={iconStyles} />,
+    'aria-label': 'Giphy tool',
   },
   {
+    icon: (
+      <ContactGroupIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
+    key: 'users',
     content: 'Users',
-    icon: <ContactGroupIcon styles={iconStyles} />,
+    'aria-label': 'Sticker Tool',
   },
   {
+    icon: (
+      <UserFriendsIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
     key: 'clients',
     content: 'Clients',
-    icon: <UserFriendsIcon styles={iconStyles} />,
+    'aria-label': 'Meetup Tool',
   },
   {
+    icon: (
+      <RobotIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
     key: 'services',
     content: 'Services',
-    icon: <RobotIcon styles={iconStyles} />,
+    'aria-label': 'Settings',
   },
   {
+    icon: (
+      <PaperclipIcon
+        {...{
+          outline: true,
+        }}
+      />
+    ),
     key: 'invoices',
-    content: 'invoices',
-    icon: <PaperclipIcon styles={iconStyles} />,
-  }
+    content: 'Invoices',
+    
+    
+  },
 ]
 
-function App() {
-  return (
-    <div className="App">
-      <Menu defaultActiveIndex={0} items={items} vertical pointing="start" />
+const MenuExampleToolbarShorthand = () => (
+  <Menu
+    vertical
+    defaultActiveIndex={0}
+    items={items}
+    iconOnly
+    accessibility={menuAsToolbarBehavior}
+    pills
+    
+  />
+)
 
-    </div>
-  );
-}
-
-export default App;
+export default MenuExampleToolbarShorthand
